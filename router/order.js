1b6a17a0de9d3e891861aea74f4cca40c64dd137
wisdom <wisdom08@kakao.com>
2022-06-29 18:26:15 +0900

    feat: 라우팅 추가

---
+++ router/order.js
@@ -0,0 +1,16 @@
+import express from "express";
+
+const router = express.Router();
+
+
+
+
+// CRUD
+
+
+
+
+
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,6 +2,9 @@ import express from 'express';
 import cors from "cors";
 import bodyParser from "body-parser";
 import morgan from "morgan";
+import orderRouter from './router/order.js';
+import productRouter from './router/product.js';
+import userRouter from './router/user.js';
 
 const app = express();
 
@@ -12,6 +15,11 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(morgan("dev"));
+
+// routing
+app.use("/user", userRouter);
+app.use("/product", productRouter);
+app.use("/order", orderRouter);
 
 const port = 3000;
 app.listen(port, () => {

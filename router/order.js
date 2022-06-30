39b8862bd7f04693b6098a5e3e2ebd9b891143ab
wisdom <wisdom08@kakao.com>
2022-06-30 23:04:49 +0900

    feat: order 라우터 추가

--- router/order.js
+++ router/order.js
@@ -1,13 +1,40 @@
 import express from "express";
+import asyncHandler from "express-async-handler";
+import orderModel from '../model/order.js'
 
 const router = express.Router();
 
 
+router.get('/', asyncHandler(async(req, res) => {
+    const orders = await orderModel.find().populate("product");
+    res.json({
+        msg: "order total get",
+        orders
+    })
+}));
 
+router.get('/:id', asyncHandler(async(req, res) => {
+    const order = await orderModel.findById(req.params.id);
+    res.json({
+        msg: "order detail get",
+        order
+    })
+}));
 
-// CRUD
+router.post('/', asyncHandler(async (req, res) => {
 
+    const {product, quantity} = req.body;
+    const newOrder = new orderModel({
+        product, quantity
+    })
 
+    const createOrder = await newOrder.save();
+
+    res.json({
+        msg: 'order post',
+        order: createOrder
+    })
+}));
 
 
 

a4fa3002f65f6883e01d5fce1206b80d227a4cda
wisdom <wisdom08@kakao.com>
2022-06-30 23:05:11 +0900

    feat: user 모델 추가

---
+++ model/user.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema(
+    {
+        email: {
+            type: String,
+            required: true
+        },
+        password: {
+            type: String,
+            required: true
+        },
+        nickname: {
+            type: String,
+            required: true
+        }
+
+    },
+    {
+        timestamps: true
+    }
+);
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
+

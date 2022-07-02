b6ef1c01917254ef98fbb5111a14949fc23030c7
wisdom <wisdom08@kakao.com>
2022-07-02 10:12:48 +0900

    feat: JWT 추가

--- controller/user.js
+++ controller/user.js
@@ -1,10 +1,12 @@
 import asyncHandler from "express-async-handler";
 import UserModel from '../model/user.js'
 import bcrypt from "bcrypt";
+import jwt from 'jsonwebtoken';
+import gravatar from 'gravatar'
 
 const saltRounds = 10;
 
-const register =  asyncHandler(async (req, res) => {
+const register = asyncHandler(async (req, res) => {
     const {email, password, nickname} = req.body;
 
     // 이미 가입된 이메일인지 체크 => password 암호화
@@ -37,25 +39,32 @@ const login = asyncHandler(async (req, res) => {
 
     const {email, password} = req.body;
 
-    // 이메일 존재 확인 => 패스워드 디코딩해서 일치 여부 확인
-    UserModel.findOne({email}, async (err, user) => {
-        if (!user) {
-            return res.json({
-                msg: "you must register"
-            })
-        }
+    // 이메일 존재 확인 => 패스워드 디코딩해서 일치 여부 확인 => return JWT!
+    const user = await UserModel.findOne({email: email});
 
-        const isMatched = await bcrypt.compare(password, user.password)
-        if (!isMatched) {
-            return res.json({
-                msg: "wrong password"
-            })
-        }
-
-        res.json({
-            msg: "login successful",
-            user: user
+    if (!user) {
+        return res.json({
+            msg: "you must register"
         })
+    }
+
+    const isMatched = await bcrypt.compare(password, user.password)
+    if (!isMatched) {
+        return res.json({
+            msg: "wrong password"
+        })
+    }
+
+    // JSON WEB TOKEN 생성
+    const token = await jwt.sign(
+        {id: user._id},
+        process.env.SECRET_OR_KEY,
+        {expiresIn: "30m"}
+    );
+
+    res.json({
+        msg: "login successful",
+        token
     })
 })
 
